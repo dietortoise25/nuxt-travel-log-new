b6c2f45f53821c16ea19525fa5d26119c5bebc47
Alan <dietortoise25@gmail.com>
2025-12-08 08:35:23 +0800

    :spackles: add a landing page

---
+++ app/components/app/nav-bar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div className="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Travel Log
+      </NuxtLink>
+    </div>
+    <div className="navbar-end space-x-2">
+      <AppThemeToggle />
+      <NuxtLink class="btn btn-accent">
+        Sign In<Icon name="tabler:brand-github" size="24" />
+      </NuxtLink>
+    </div>
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hero container mx-auto bg-base-300 mt-4">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Keep track of your adventures and explore new destinations with Travel Log, your personal travel companion.
+        </p>
+        <button class="btn btn-accent">
+          Sign In with Github<Icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
