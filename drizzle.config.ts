be488b99fdf8e4a1a1ae5469c2c81300e80eed15
Alan <dietortoise25@gmail.com>
2025-12-09 06:55:47 +0800

    :sparkles: setup db

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "drizzle-kit";
+
+import env from "./shared/utils/env";
+
+export default defineConfig({
+  out: "./server/db/migrations",
+  schema: "./server/db/schema",
+  casing: "snake_case",
+  dialect: "turso",
+  dbCredentials: {
+    url: env.TURSO_DATABASE_URL,
+    authToken: env.NODE_ENV === "development"
+      ? undefined
+      : env.TURSO_AUTH_TOKEN,
+  },
+});
