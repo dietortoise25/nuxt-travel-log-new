abc088c642e65f07b67b0a9baacb0fe15d85f8b4
dietortoise25 <dietortoise25@gmail.com>
2025-12-10 23:53:38 +0800

    :spackles: authentication status in the UI / sign out (#56)
    * :spackles: authentication status in the UI / sign out
    
    * fixed: workflow env

---
+++ app/pages/sign-out.vue
@@ -0,0 +1,12 @@
+<script langt="ts" setup>
+const { signOut } = useAuthStore();
+onMounted(() => {
+  signOut();
+});
+</script>
+
+<template>
+  <div class="card bg-base-300 container mt-4 min-h-72 text-center mx-auto flex flex-col gap-4 justify-center items-center">
+    <span class="loading loading-spinner loading-xl" />
+  </div>
+</template>
