4030a2e845af1331114a8834d362791b701f913f
dietortoise25 <dietortoise25@gmail.com>
2025-12-11 00:23:49 +0800

    :sparkles: protect routes (#58)

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+</script>
+
 <template>
   <div class="hero container mx-auto bg-base-300 mt-4">
     <div class="hero-content text-center min-h-96">
@@ -8,7 +12,10 @@
         <p class="py-6">
           Keep track of your adventures and explore new destinations with Travel Log, your personal travel companion.
         </p>
-        <AuthButton />
+        <AuthButton v-if="!authStore.user" />
+        <NuxtLink v-else to="/dashboard" class="btn btn-primary">
+          Go to Dashboard
+        </NuxtLink>
       </div>
     </div>
   </div>
