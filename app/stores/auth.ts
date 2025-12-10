79897effa882bb62ababad459065cc11f93dad18
Alan <dietortoise25@gmail.com>
2025-12-10 23:36:53 +0800

    :spackles: authentication status in the UI / sign out

--- app/stores/auth.ts
+++ app/stores/auth.ts
@@ -1,20 +1,39 @@
-import { createAuthClient } from "better-auth/client";
+import { createAuthClient } from "better-auth/vue";
 
 const authClient = createAuthClient();
 
 export const useAuthStore = defineStore("authStore", () => {
+  const session = authClient.useSession();
+  const user = computed(() => session.value.data?.user);
   const loading = ref(false);
+
   async function signIn() {
     loading.value = true;
-    await authClient.signIn.social({
-      provider: "github",
-      callbackURL: "/dashboard",
-      errorCallbackURL: "/error",
-    });
-    loading.value = false;
+    await nextTick();
+    try {
+      await authClient.signIn.social({
+        provider: "github",
+        callbackURL: "/dashboard",
+        errorCallbackURL: "/error",
+      });
+    }
+    catch (error) {
+      console.error("Failed to load providers:", error);
+    }
+    finally {
+      loading.value = false;
+    }
   }
+
+  async function signOut() {
+    await authClient.signOut();
+    navigateTo("/");
+  }
+
   return {
     loading,
     signIn,
+    user,
+    signOut,
   };
 });
