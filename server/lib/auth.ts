2051fe9dd70bbd263dbe707d5e23161ec4e36718
Alan <dietortoise25@gmail.com>
2025-12-11 21:38:49 +0800

    Auth Session Setup

--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -1,10 +1,23 @@
 import env from "#shared/utils/env";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { createAuthMiddleware } from "better-auth/plugins";
 
 import db from "../db";
 
 export const auth = betterAuth({
+  hooks: {
+    after: createAuthMiddleware(async (ctx) => {
+      if (ctx.path === "get-session") {
+        if (!ctx.context.session) {
+          return ctx.json({
+            session: null,
+            user: null,
+          });
+        }
+      }
+    }),
+  },
   database: drizzleAdapter(db, {
     provider: "sqlite", // or "mysql", "pg"
   }),
