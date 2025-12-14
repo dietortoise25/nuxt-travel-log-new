7e445dbdda90a9ebd696cf9edfd83f98284c548a
dietortoise25 <dietortoise25@gmail.com>
2025-12-14 22:38:50 +0800

    :sparkles: add sidebar locations (#67)

--- server/api/locations.get.ts
+++ server/api/locations.get.ts
@@ -1,7 +1,9 @@
-import { getLocationsByUserId } from "../db/queries/location";
+import { getLocationsByUserId as findLocationsByUserId } from "../db/queries/location";
 import defineAuthenticatedEventHandler from "../utils/define-authenticated-event-handler";
 
 export default defineAuthenticatedEventHandler(async (event) => {
+  // await new Promise(resolve => setTimeout(resolve, 2000));
+
   // 获取当前用户的所有位置
-  return getLocationsByUserId(event.context.user.id);
+  return findLocationsByUserId(event.context.user.id);
 });
