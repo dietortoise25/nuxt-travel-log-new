ac82575500269b998811d72a8bc62b5e5b0aed39
Alan <dietortoise25@gmail.com>
2025-12-18 01:36:06 +0800

    fixed some ts issue. (#75)

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -4,7 +4,12 @@ export default defineEventHandler(async (event) => {
   const session = await auth.api.getSession({
     headers: event.headers,
   });
-  event.context.user = session?.user;
+  event.context.user = session?.user
+    ? {
+        ...session.user,
+        id: Number.parseInt(session.user.id),
+      }
+    : undefined;
   if (event.path.startsWith("/dashboard")) {
     if (!session?.user) {
       await sendRedirect(event, "/", 302);
